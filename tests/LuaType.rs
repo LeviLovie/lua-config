0496db3e1bddf68cb5073f7e299463da1b335037
LeviLovie <107021730+LeviLovie@users.noreply.github.com>
2024-08-14 14:38:29 +0300

    Fix tests AGAIN

--- tests/LuaType.rs
+++ tests/LuaType.rs
@@ -3,40 +3,40 @@ mod lua_type {
     #[test]
     fn integer() {
         let lua_type = lua_config::LuaType::Integer(42);
-        assert_eq!(lua_type.get::<i32>(), Some(42));
-        assert_eq!(lua_type.get::<i64>(), Some(42));
-        assert_eq!(lua_type.get::<f64>(), Some(42.0));
-        assert_eq!(lua_type.get::<bool>(), None);
-        assert_eq!(lua_type.get::<String>(), None);
+        assert_eq!(lua_type.to::<i32>(), Some(42));
+        assert_eq!(lua_type.to::<i64>(), Some(42));
+        assert_eq!(lua_type.to::<f64>(), Some(42.0));
+        assert_eq!(lua_type.to::<bool>(), None);
+        assert_eq!(lua_type.to::<String>(), None);
     }
 
     #[test]
     fn number() {
         let lua_type = lua_config::LuaType::Number(42.0);
-        assert_eq!(lua_type.get::<i32>(), Some(42));
-        assert_eq!(lua_type.get::<i64>(), Some(42));
-        assert_eq!(lua_type.get::<f64>(), Some(42.0));
-        assert_eq!(lua_type.get::<bool>(), None);
-        assert_eq!(lua_type.get::<String>(), None);
+        assert_eq!(lua_type.to::<i32>(), Some(42));
+        assert_eq!(lua_type.to::<i64>(), Some(42));
+        assert_eq!(lua_type.to::<f64>(), Some(42.0));
+        assert_eq!(lua_type.to::<bool>(), None);
+        assert_eq!(lua_type.to::<String>(), None);
     }
 
     #[test]
     fn boolean() {
         let lua_type = lua_config::LuaType::Boolean(true);
-        assert_eq!(lua_type.get::<i32>(), None);
-        assert_eq!(lua_type.get::<i64>(), None);
-        assert_eq!(lua_type.get::<f64>(), None);
-        assert_eq!(lua_type.get::<bool>(), Some(true));
-        assert_eq!(lua_type.get::<String>(), None);
+        assert_eq!(lua_type.to::<i32>(), None);
+        assert_eq!(lua_type.to::<i64>(), None);
+        assert_eq!(lua_type.to::<f64>(), None);
+        assert_eq!(lua_type.to::<bool>(), Some(true));
+        assert_eq!(lua_type.to::<String>(), None);
     }
 
     #[test]
     fn string() {
         let lua_type = lua_config::LuaType::String("Hello, World!".to_string());
-        assert_eq!(lua_type.get::<i32>(), None);
-        assert_eq!(lua_type.get::<i64>(), None);
-        assert_eq!(lua_type.get::<f64>(), None);
-        assert_eq!(lua_type.get::<bool>(), None);
-        assert_eq!(lua_type.get::<String>(), Some("Hello, World!".to_string()));
+        assert_eq!(lua_type.to::<i32>(), None);
+        assert_eq!(lua_type.to::<i64>(), None);
+        assert_eq!(lua_type.to::<f64>(), None);
+        assert_eq!(lua_type.to::<bool>(), None);
+        assert_eq!(lua_type.to::<String>(), Some("Hello, World!".to_string()));
     }
 }
